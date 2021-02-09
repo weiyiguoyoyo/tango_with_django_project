b649bdc9100cd15cd1af0bfdc23edbd7c7c9b478
Weiyi Guo <2469211g@student.gla.ac.uk>
2021-02-09 17:34:49 +0000

    first commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -79,8 +79,16 @@ TEMPLATES = [
     },
 ]
 
+PASSWORD_HASHERS = (
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+        'django.contrib.auth.hashers.BCryptPasswordHasher',
+    )
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
+LOGIN_URL = '/rango/login/'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
@@ -102,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
